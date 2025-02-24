575f2c64aea5f4e281b55656d307b3d96950add6
daniel <danilabradaperez04@gmail.com>
2025-02-24 09:44:27 -0700

    cambios para el examen

--- app/src/main/java/mx/itson/cheems/MainActivity.kt
+++ app/src/main/java/mx/itson/cheems/MainActivity.kt
@@ -8,6 +8,7 @@ import android.os.Vibrator
 import android.os.VibratorManager
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -16,83 +17,114 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    var gameOverCard = 0
+    private var gameOverCard = 0
+    private var flippedCards = mutableSetOf<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        val buttonFlip = findViewById<Button>(R.id.button_flip)
+        buttonFlip.setOnClickListener {
+            flipAllCards()
+        }
+
+
+        Toast.makeText(this, "Bienvenido al juego", Toast.LENGTH_LONG).show()
+
+
         start()
     }
 
-    fun start(){
-        for(i in 1 .. 6){
+
+    fun start() {
+        flippedCards.clear()
+        for (i in 1..12) {
             val btnCard = findViewById<View>(
                 resources.getIdentifier("card$i", "id", this.packageName)
             ) as ImageButton
             btnCard.setOnClickListener(this)
             btnCard.setBackgroundResource(R.drawable.icon_pregunta)
         }
-        gameOverCard = (1 .. 6).random()
-
-        Log.d("El valor de la carta", "La carta perdedora es ${gameOverCard}")
+        gameOverCard = (1..12).random()
+        Log.d("El valor de la carta", "La carta perdedora es $gameOverCard")
     }
 
-    fun flip(card : Int){
-        if(card == gameOverCard){
-            //ya perdio
+    fun flip(card: Int) {
+        if (card == gameOverCard) {
+            vibrateDevice()
+            Toast.makeText(this, getString(R.string.text_game_over), Toast.LENGTH_LONG).show()
 
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S){
-                // si la version de android del usuario es mayor o igual a 12
-                val vibratorAdmin = applicationContext.getSystemService((Context.VIBRATOR_MANAGER_SERVICE)) as VibratorManager
-                val vibrator = vibratorAdmin.defaultVibrator
-                vibrator.vibrate(VibrationEffect.createOneShot(1000, VibrationEffect.DEFAULT_AMPLITUDE))
-
-            }else {
-                val vibrator = applicationContext.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
-                vibrator.vibrate(1000)
-                
-            }
-
-            Toast.makeText(this,getString(R.string.text_game_over), Toast.LENGTH_LONG).show()
-
-            for(i in 1..6){
-
+            for (i in 1..12) {
                 val btnCard = findViewById<View>(
-                    resources.getIdentifier( "card$card", "id", this.packageName )
-                )as ImageButton
-                if (i == card){
+                    resources.getIdentifier("card$i", "id", this.packageName)
+                ) as ImageButton
+                if (i == card) {
                     btnCard.setBackgroundResource(R.drawable.icon_chempe)
-                }else{
+                } else {
                     btnCard.setBackgroundResource(R.drawable.icon_cheems)
                 }
-
             }
-        }else{
-            //continua el juego
+        } else {
             val btnCard = findViewById<View>(
-                resources.getIdentifier( "card$card", "id", this.packageName )
-            )as ImageButton
+                resources.getIdentifier("card$card", "id", this.packageName)
+            ) as ImageButton
+            btnCard.setBackgroundResource(R.drawable.icon_cheems)
+            flippedCards.add(card)
+
+            
+            if (flippedCards.size == 11) {
+                Toast.makeText(this, getString(R.string.text_win), Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    fun flipAllCards() {
+        for (i in 1..12) {
+            val btnCard = findViewById<ImageButton>(
+                resources.getIdentifier("card$i", "id", packageName)
+            )
             btnCard.setBackgroundResource(R.drawable.icon_cheems)
         }
     }
 
+
+    private fun vibrateDevice() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val vibratorAdmin =
+                applicationContext.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+            val vibrator = vibratorAdmin.defaultVibrator
+            vibrator.vibrate(VibrationEffect.createOneShot(1000, VibrationEffect.DEFAULT_AMPLITUDE))
+        } else {
+            val vibrator = applicationContext.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+            vibrator.vibrate(1000)
+        }
+    }
+
     override fun onClick(v: View) {
-        when(v.id){
-            R.id.card1 -> { flip(1)}
-            R.id.card2 -> { flip(2)}
-            R.id.card3 -> { flip(3)}
-            R.id.card4 -> { flip(4)}
-            R.id.card5 -> { flip(5)}
-            R.id.card6 -> { flip(6)}
-
-
+        when (v.id) {
+            R.id.card1 -> flip(1)
+            R.id.card2 -> flip(2)
+            R.id.card3 -> flip(3)
+            R.id.card4 -> flip(4)
+            R.id.card5 -> flip(5)
+            R.id.card6 -> flip(6)
+            R.id.card7 -> flip(7)
+            R.id.card8 -> flip(8)
+            R.id.card9 -> flip(9)
+            R.id.card10 -> flip(10)
+            R.id.card11 -> flip(11)
+            R.id.card12 -> flip(12)
 
         }
-        }
+    }
 }
